62419c3bcf21970858756029a4af99819bba7064
Rychen Jones <rychenjones66@gmail.com>
2026-09-18 19:24:12 -0600

    fixed link

--- src/js/modules/pocketbase.js
+++ src/js/modules/pocketbase.js
@@ -1,5 +1,10 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('http://127.0.0.1:8090');
+const PB_URL =
+  window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://127.0.0.1:8090'
+    : 'https://repjournal.duckdns.org';
+
+const pb = new PocketBase(PB_URL);
 
 export default pb;
